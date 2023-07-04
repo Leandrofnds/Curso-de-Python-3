bc32fbcb540eb9c1734e43de98e7ca95f98ba0df
Leandrofnds <leandrofernandes.saraiva@gmail.com>
2023-07-04 16:37:39 -0300

    update

---
+++ sessao3/aula12.py
@@ -0,0 +1,11 @@
+#Exercicio de IMC
+
+"""
+Insercao das variaveis que serao utilizadas para realizar a soma
+"""
+nome = "Leandro Fernandes"
+altura = 1.70
+peso = 80
+imc =  int(peso // (altura*altura))
+
+print(nome, "tem", altura, "de altura,", "pesa", peso, "quilos e seu IMC e", imc)
